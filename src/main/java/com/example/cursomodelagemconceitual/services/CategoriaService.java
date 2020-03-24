e70b55accc81e3e9da2a1d286e0320eeaec5daa6
Edberto Leonardo <edbleonardo@gmail.com>
2020-03-24 11:20:43 -0300

    Listando todas categorias com DTO

--- src/main/java/com/example/cursomodelagemconceitual/services/CategoriaService.java
+++ src/main/java/com/example/cursomodelagemconceitual/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.example.cursomodelagemconceitual.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,4 +43,7 @@ public class CategoriaService {
 		}
 	}
 
+	public List<Categoria> findAll(){
+		return categoriaRepository.findAll();
+	}
 }
