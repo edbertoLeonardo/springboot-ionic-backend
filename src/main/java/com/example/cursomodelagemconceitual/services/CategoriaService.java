9c2a746ab7b19abb3f64ff1b6b35792247e335f8
Edberto Leonardo <edbleonardo@gmail.com>
2020-03-24 12:25:50 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/example/cursomodelagemconceitual/services/CategoriaService.java
+++ src/main/java/com/example/cursomodelagemconceitual/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.example.cursomodelagemconceitual.domain.Categoria;
+import com.example.cursomodelagemconceitual.dto.CategoriaDTO;
 import com.example.cursomodelagemconceitual.repositories.CategoriaRepository;
 import com.example.cursomodelagemconceitual.services.exception.DataIntegretyException;
 import com.example.cursomodelagemconceitual.services.exception.ObjectNotFoundException;
@@ -53,5 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO categoriaDto) {
+		return new Categoria(categoriaDto.getId(), categoriaDto.getNome());
+		
 	}
 }
